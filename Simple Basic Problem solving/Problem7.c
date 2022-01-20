5bab75ef1b4d2b31fc1cefb3a0fbc5ecf3e3fc55
ST <sadadrana@gmail.com>
2022-01-20 18:36:00 +0600

    Just Commit

---
+++ Simple Basic Problem solving/Problem7.c
@@ -0,0 +1,50 @@
+
+
+#include <stdio.h>
+
+int main()
+{
+  float salary,earned,newsalary;
+  scanf("%f",&salary); 
+if(salary>=0 && salary<=400)
+{
+  earned=salary*(15.0/100.0);
+  newsalary=salary+earned;
+  printf("New Salary: %.2f\n",newsalary);
+  printf("Money earned: %.2f\n",earned);
+  printf("In percentage: 15");
+}
+else if(salary>=400.01 && salary<=800)
+{
+  earned=(salary*(12.0/100.0));
+  newsalary=salary+earned;
+  printf("New Salary: %.2f\n",newsalary);
+  printf("Money earned: %.2f\n",earned);
+  printf("In percentage: 12");
+}
+else if(salary>=800.01 && salary<=1200)
+{
+  earned=salary*(10.0/100.0);
+  newsalary=salary+earned;
+  printf("New Salary: %.2f\n",newsalary);
+  printf("Money earned: %.2f\n",earned);
+  printf("In percentage: 10");
+}
+else if(salary>=1200.01 && salary<=2000)
+{
+  earned=salary*(7.0/100.0);
+  newsalary=salary+earned;
+  printf("New Salary: %.2f\n",newsalary);
+  printf("Money earned: %.2f\n",earned);
+  printf("In percentage: 7");
+}
+else
+{
+  earned=salary*(4.0/100.0);
+  newsalary=salary+earned;
+  printf("New Salary: %.2f\n",newsalary);
+  printf("Money earned: %.2f\n",earned);
+  printf("In percentage: 4");
+}
+return 0;
+}
