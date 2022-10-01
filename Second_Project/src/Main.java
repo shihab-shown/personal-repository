644053f01b99e2bc802ef2c9c4b264925d65bfd1
Shihab Tawsif <sadadrana@gmail.com>
2022-10-01 12:34:47 +0600

    Just Commit

--- Second_Project/src/Main.java
+++ Second_Project/src/Main.java
@@ -1,8 +1,7 @@
-import java.awt.Color;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
-public class Main {
+public class What {
   public static void main(String[] args){
     JFrame frame = new JFrame();
     frame.setVisible(true);
