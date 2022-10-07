1a920b29c9f8ea20b15293b4ce25c950ac31f97f
Shihab Tawsif <sadadrana@gmail.com>
2022-10-08 01:35:25 +0600

    Just Commit

--- JAVA/Frame/src/Main.java
+++ JAVA/Frame/src/Main.java
@@ -12,6 +12,9 @@ public class Main extends JFrame {
         setVisible(true);
     }
     public static void main(String[] args) {
-        Main mainframe = new Main();
+        //Main mainframe = new Main();
+        System.out.println("Hello World");int x1 = 4, x2 = 13, y1 = 7, y2 = 21;
+        int result = Math.abs(x1-x2) + Math.abs(y1-y2);
+        System.out.println("The Manhattan Distance is: "+result);
     }
 }
