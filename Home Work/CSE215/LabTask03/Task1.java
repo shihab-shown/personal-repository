5b8f0ef0f92a847322abc2ce99724ab0fd26c318
Shihab Tawsif <sadadrana@gmail.com>
2022-10-13 20:36:41 +0600

    Just Commit

--- Home Work/CSE215/LabTask03/Task1.java
+++ Home Work/CSE215/LabTask03/Task1.java
@@ -7,11 +7,11 @@ public class Task1 {
         System.out.println(num2);
         System.out.println(num3);
         if(num1<num2 && num1<num3){
-            System.out.println(num1); 
+            System.out.println(num1+" is the smallest"); 
         } else if (num2<num3) {
-            System.out.println(num2);
+            System.out.println(num2+" is the smallest");
         } else {
-            System.out.println(num3);
+            System.out.println(num3+" is the smallest");
         }
     }
 }
