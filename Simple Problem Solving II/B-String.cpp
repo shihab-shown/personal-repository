24c0fe89ff343639707c2a0afd145db5d608823a
Shihab Tawsif <sadadrana@gmail.com>
2022-11-14 17:10:25 +0600

    Just Commit

--- Simple Problem Solving II/B-String.cpp
+++ Simple Problem Solving II/B-String.cpp
@@ -1,11 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 int main()
 {
   string a,b;
   cin>>a>>b;
-  cout<<a<<endl;
-  cout<<b<<endl;
+  cout<<a.length()<<' '<<b.length()<<endl;
+  cout<<a+b<<endl;
+  char temp;
+  temp = a[0];
+  a[0] = b[0];
+  b[0] = temp;
+  cout<<a<<' '<<b<<endl;
   return 0;
 }
